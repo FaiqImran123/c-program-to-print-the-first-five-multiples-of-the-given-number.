7b98feee52275a61275a4d8b364ccaeef9ef4081
FaiqImran123 <87329733+FaiqImran123@users.noreply.github.com>
2021-12-02 14:47:20 +0500

    Add files via upload

---
+++ main (4).cpp
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int num;
+cout<<"Enter Number:";
+cin>>num;
+cout<<"Numbers are: "<<num<< " "<<num*2<<" "<<num*3<<" "<<num*4<<" "<<num*5; 
+}
